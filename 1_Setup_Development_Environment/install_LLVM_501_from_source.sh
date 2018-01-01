fb28ac81c03a1079504ab06df36f2996d0bec450
Matthew J. Riblett <riblettmj@vcu.edu>
2018-01-01 14:20:12 -0500

    WIP -- Added script for downloading, building, and installing LLVM 5.0.1 from source ** Commented out additional downloads in LLVM 5.0.0 install script

--- 1_Setup_Development_Environment/install_LLVM_501_from_source.sh
+++ 1_Setup_Development_Environment/install_LLVM_501_from_source.sh
@@ -2,10 +2,10 @@
 # ==============================================================================
 # LLVM INSTALL SCRIPT:
 #  Written By:  Matthew J. Riblett
-#  Rev. Date:   October 25th, 2017
+#  Rev. Date:   January 1st, 2018
 
 # PURPOSE:
-#  This script automates the configuration and installation of the LLVM 5.0.0
+#  This script automates the configuration and installation of the LLVM 5.0.1
 #  compiler infrastructure on a UNIX system.  The purpose of this script is to
 #  provide an means of achieving a consistent installation across all targets.
 
@@ -20,7 +20,8 @@
 #  on a non-Ubuntu system.
 
 # LICENSE AND COPYRIGHT NOTICE:
-#  LLVM Installation Helper Script
+#
+#  ribPy2 -- High Performance Python Package for Medical Physics
 #  Copyright (C) 2017 Matthew J. Riblett
 #
 #  This program is free software: you can redistribute it and/or modify
@@ -69,7 +70,7 @@ fi
 echo -e "\n\033[42m                                                                                \033[0m"
 echo -e "\033[47;30m                     LLVM INSTALL SCRIPT :: M.J.Riblett                         \033[0m"
 echo -e "\033[42m                                                                                \033[0m"
-echo -e "\n This script automates the configuration and installation of the LLVM 5.0.0"
+echo -e "\n This script automates the configuration and installation of the LLVM 5.0.1"
 echo -e " compiler infrastructure on a UNIX system.  The purpose of this script is to"
 echo -e " provide an means of achieving a consistent installation across all targets."
 echo -e "\n >> Press [ENTER] to proceed with installation or [Ctrl+C] to quit."
@@ -116,81 +117,58 @@ fi
 echo -ne '\n\033[32m Downloading required source files\033[0m\n'
 cd ${workingPath}
 mkdir -p llvm && cd llvm > /dev/null 2>&1
-mkdir -p llvm-5.0.0.build > /dev/null 2>&1
+mkdir -p llvm-5.0.1.build > /dev/null 2>&1
 echo -ne ' - Downloading LLVM core sources\n'
-wget http://releases.llvm.org/5.0.0/llvm-5.0.0.src.tar.xz > /dev/null 2>&1
-tar xf llvm-5.0.0.src.tar.xz; rm llvm-5.0.0.src.tar.xz > /dev/null 2>&1
-cd llvm-5.0.0.src > /dev/null 2>&1
+wget http://releases.llvm.org/5.0.1/llvm-5.0.1.src.tar.xz > /dev/null 2>&1
+tar xf llvm-5.0.1.src.tar.xz; rm llvm-5.0.1.src.tar.xz > /dev/null 2>&1
+cd llvm-5.0.1.src > /dev/null 2>&1
 
 
-# Download and extract tool dependency sources
+# Download and extract dependency sources
 # ------------------------------------------------------------------------------
 cd tools > /dev/null 2>&1
 echo -ne ' - Downloading Clang sources\n'
-wget http://releases.llvm.org/5.0.0/cfe-5.0.0.src.tar.xz > /dev/null 2>&1
-mkdir -p clang && tar xf cfe-5.0.0.src.tar.xz -C clang --strip-components=1 > /dev/null 2>&1
-rm cfe-5.0.0.src.tar.xz > /dev/null 2>&1
+wget http://releases.llvm.org/5.0.1/cfe-5.0.1.src.tar.xz > /dev/null 2>&1
+mkdir -p clang && tar xf cfe-5.0.1.src.tar.xz -C clang --strip-components=1 > /dev/null 2>&1
+rm cfe-5.0.1.src.tar.xz > /dev/null 2>&1
 
-#echo -ne ' - Downloading LDD sources\n'
-#wget http://releases.llvm.org/5.0.0/lld-5.0.0.src.tar.xz > /dev/null 2>&1
-#mkdir -p lld && tar xf lld-5.0.0.src.tar.xz -C ldd --strip-components=1 > /dev/null 2>&1
-#rm lld-5.0.0.src.tar.xz > /dev/null 2>&1
-
-
-# Download and extract project dependency sources
-# ------------------------------------------------------------------------------
 cd ../projects
 echo -ne ' - Downloading Compiler-RT sources\n'
-wget http://releases.llvm.org/5.0.0/compiler-rt-5.0.0.src.tar.xz > /dev/null 2>&1
-mkdir -p compiler-rt && tar xf compiler-rt-5.0.0.src.tar.xz -C compiler-rt --strip-components=1 > /dev/null 2>&1
-rm compiler-rt-5.0.0.src.tar.xz > /dev/null 2>&1
+wget http://releases.llvm.org/5.0.1/compiler-rt-5.0.1.src.tar.xz > /dev/null 2>&1
+mkdir -p compiler-rt && tar xf compiler-rt-5.0.1.src.tar.xz -C compiler-rt --strip-components=1 > /dev/null 2>&1
+rm compiler-rt-5.0.1.src.tar.xz > /dev/null 2>&1
 
 echo -ne ' - Downloading libCXX sources\n'
-wget http://releases.llvm.org/5.0.0/libcxx-5.0.0.src.tar.xz > /dev/null 2>&1
-mkdir -p libcxx && tar xf libcxx-5.0.0.src.tar.xz -C libcxx --strip-components=1 > /dev/null 2>&1
-rm libcxx-5.0.0.src.tar.xz > /dev/null 2>&1
+wget http://releases.llvm.org/5.0.1/libcxx-5.0.1.src.tar.xz > /dev/null 2>&1
+mkdir -p libcxx && tar xf libcxx-5.0.1.src.tar.xz -C libcxx --strip-components=1 > /dev/null 2>&1
+rm libcxx-5.0.1.src.tar.xz > /dev/null 2>&1
 
 echo -ne ' - Downloading libCXX-ABI sources\n'
-wget http://releases.llvm.org/5.0.0/libcxxabi-5.0.0.src.tar.xz > /dev/null 2>&1
-mkdir -p libcxxabi && tar xf libcxxabi-5.0.0.src.tar.xz -C libcxxabi --strip-components=1 > /dev/null 2>&1
-rm libcxxabi-5.0.0.src.tar.xz > /dev/null 2>&1
+wget http://releases.llvm.org/5.0.1/libcxxabi-5.0.1.src.tar.xz > /dev/null 2>&1
+mkdir -p libcxxabi && tar xf libcxxabi-5.0.1.src.tar.xz -C libcxxabi --strip-components=1 > /dev/null 2>&1
+rm libcxxabi-5.0.1.src.tar.xz > /dev/null 2>&1
 
 echo -ne ' - Downloading libunwind sources\n'
-wget http://releases.llvm.org/5.0.0/libunwind-5.0.0.src.tar.xz > /dev/null 2>&1
-mkdir -p libunwind && tar xf libunwind-5.0.0.src.tar.xz -C libunwind --strip-components=1 > /dev/null 2>&1
-rm libunwind-5.0.0.src.tar.xz > /dev/null 2>&1
-
-#echo -ne ' - Downloading LDDB sources\n'
-#wget http://releases.llvm.org/5.0.0/lldb-5.0.0.src.tar.xz > /dev/null 2>&1
-#mkdir -p lldb && tar xf lldb-5.0.0.src.tar.xz -C lddb --strip-components=1 > /dev/null 2>&1
-#rm lldb-5.0.0.src.tar.xz > /dev/null 2>&1
+wget http://releases.llvm.org/5.0.1/libunwind-5.0.1.src.tar.xz > /dev/null 2>&1
+mkdir -p libunwind && tar xf libunwind-5.0.1.src.tar.xz -C libunwind --strip-components=1 > /dev/null 2>&1
+rm libunwind-5.0.1.src.tar.xz > /dev/null 2>&1
 
 echo -ne ' - Downloading OpenMP bindings sources\n'
-wget http://releases.llvm.org/5.0.0/openmp-5.0.0.src.tar.xz > /dev/null 2>&1
-mkdir -p openmp && tar xf openmp-5.0.0.src.tar.xz -C openmp --strip-components=1 > /dev/null 2>&1
-rm openmp-5.0.0.src.tar.xz > /dev/null 2>&1
-
-#echo -ne ' - Downloading Polly sources\n'
-#wget http://releases.llvm.org/5.0.0/polly-5.0.0.src.tar.xz > /dev/null 2>&1
-#mkdir -p polly && tar xf polly-5.0.0.src.tar.xz -C polly --strip-components=1 > /dev/null 2>&1
-#rm polly-5.0.0.src.tar.xz > /dev/null 2>&1
-
-#echo -ne ' - Downloading Clang Tools Extra sources\n'
-#wget http://releases.llvm.org/5.0.0/clang-tools-extra-5.0.0.src.tar.xz > /dev/null 2>&1
-#mkdir -p clang-tools-extra && tar xf clang-tools-extra-5.0.0.src.tar.xz -C clang-tools-extra --strip-components=1 > /dev/null 2>&1
-#rm clang-tools-extra-5.0.0.src.tar.xz > /dev/null 2>&1
+wget http://releases.llvm.org/5.0.1/openmp-5.0.1.src.tar.xz > /dev/null 2>&1
+mkdir -p openmp && tar xf openmp-5.0.1.src.tar.xz -C openmp --strip-components=1 > /dev/null 2>&1
+rm openmp-5.0.1.src.tar.xz > /dev/null 2>&1
 
 echo -ne ' - Downloading LLVM test suite sources\n'
-wget http://releases.llvm.org/5.0.0/test-suite-5.0.0.src.tar.xz > /dev/null 2>&1
-mkdir -p test-suite && tar xf test-suite-5.0.0.src.tar.xz -C test-suite --strip-components=1 > /dev/null 2>&1
-rm test-suite-5.0.0.src.tar.xz > /dev/null 2>&1
+wget http://releases.llvm.org/5.0.1/test-suite-5.0.1.src.tar.xz > /dev/null 2>&1
+mkdir -p test-suite && tar xf test-suite-5.0.1.src.tar.xz -C test-suite --strip-components=1 > /dev/null 2>&1
+rm test-suite-5.0.1.src.tar.xz > /dev/null 2>&1
 
 
 # Configure LLVM build
 # ------------------------------------------------------------------------------
 echo -ne '\033[32m Configuring LLVM build\033[0m\n'
-cd ../../llvm-5.0.0.build > /dev/null 2>&1
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ../llvm-5.0.0.src/ > /dev/null 2>&1
+cd ../../llvm-5.0.1.build > /dev/null 2>&1
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ../llvm-5.0.1.src/ > /dev/null 2>&1
 
 
 # Build and Install
@@ -204,12 +182,12 @@ if [ "$STATUS" -eq 0 ]; then
   sudo make install > /dev/null 2>&1
   STATUS=$?
   if [ "$STATUS" -eq 0 ]; then
-    echo -e "\n\033[32m SUCCESS: \033[0m Installed LLVM 5.0.0."
+    echo -e "\n\033[32m SUCCESS: \033[0m Installed LLVM 5.0.1."
   else
-    echo -e "\n\033[31m FAILED: \033[0m Error installing LLVM 5.0.0."
+    echo -e "\n\033[31m FAILED: \033[0m Error installing LLVM 5.0.1."
   fi
 else
-  echo -e "\n\033[31m FAILED: \033[0m Error building LLVM 5.0.0."
+  echo -e "\n\033[31m FAILED: \033[0m Error building LLVM 5.0.1."
 fi
 
 # ==============================================================================
